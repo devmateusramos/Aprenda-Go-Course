73ec4023d954282e15c8097a80f72ce9982ac041
devmateusramos <devmateusramos@gmail.com>
2022-01-08 18:10:23 -0300

    exercises final

---
+++ 4.4 exercise 5/go.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type creation int
+
+var x creation
+
+var y int
+
+func main() {
+	fmt.Printf("%v\n", x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Printf("%v\n", x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
+}
